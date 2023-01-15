83691bd419a235f61eece06adafb9adaff4991c2
NickBwalley <nickbwalley@gmail.com>
2023-01-15 08:49:17 +0300

    crud react-typescript

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-interface Props {
+interface props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
   handleAdd: (e: React.FormEvent) => void;
 }
-const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={(e) => handleAdd(e)}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
-        type="input"
+        type="text"
+        placeholder="Enter a Task"
         value={todo}
+        ref={inputRef}
         onChange={(e) => setTodo(e.target.value)}
-        placeholder="Enter a task"
         className="input__box"
       />
-      <button className="input_submit">Go </button>
+      <button type="submit" className="input_submit">
+        GO
+      </button>
     </form>
   );
 };
