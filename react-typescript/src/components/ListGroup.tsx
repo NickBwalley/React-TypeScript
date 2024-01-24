78c07b6a2908c7c32c4f3ed9ab7eec0c6b204db9
NickBwalley <nickbwalley@gmail.com>
2024-01-24 12:05:48 +0300

    conditional-rendering

--- react-typescript/src/components/ListGroup.tsx
+++ react-typescript/src/components/ListGroup.tsx
@@ -1,11 +1,18 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  const getMessage = () => {
+    // return items.length === 0 ? <p>No items found!</p> : null;
+    return items.length === 0 && <p>No items found!</p>;
+  };
 
   return (
     <>
       <h1>List of Items</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
